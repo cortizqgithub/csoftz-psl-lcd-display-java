5244801b0169977b18f3d5a83934d4e285c80408
Carlos Adolfo Ortiz Q (Arkix) <carlosaortiz@yahoo.com>
2016-09-19 21:33:49 -0500

    Adds initial Spring Boot solution files.

---
+++ src/main/java/com/csoftz/psl/lcd/display/CsoftzPslLcdDisplayJavaApplication.java
@@ -0,0 +1,12 @@
+package com.csoftz.psl.lcd.display;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsoftzPslLcdDisplayJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsoftzPslLcdDisplayJavaApplication.class, args);
+	}
+}
